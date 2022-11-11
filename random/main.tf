fa3908e18e255b77a988e518dfcab895f4187d01
Ash Rajagopal <ash.rajagopal@hashicorp.com>
2022-11-11 13:13:26 -0700

    update

--- random/main.tf
+++ random/main.tf
@@ -2,14 +2,14 @@ resource "random_pet" "animal_one" {
   keepers = {
     uuid = "${uuid()}" # Force a new name each time
   }
-  length = 3
+  length = 5
 }
 
 resource "random_pet" "animal_two" {
   keepers = {
     uuid = "${uuid()}" # Force a new name each time
   }
-  length = 2
+  length = 3
 }
 
 resource "random_pet" "animal_three" {
